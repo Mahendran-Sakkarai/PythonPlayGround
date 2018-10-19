42f36ff1333f4f43119943fac15f995c08975d9f
Mahendran Sakkarai <mahendranmails@gmail.com>
2018-10-19 18:51:10 +0530

    Playing with user input

---
+++ user_input/ex1.py
@@ -0,0 +1,10 @@
+import sys
+# ----------------------------   To read String   ---------------------
+# All input read as string. If it's required we need to cast it explicitly.
+name = input("What's your name? >> ")
+
+# ----------------------------   To read Integer   -------------------
+# After getting the input casting that to integer.
+age = int(input("What's your age? >> "))
+
+print("So! Your name is \"" + name + "\" and your age is \""+ str(age) + "\". Hmm. Thanks for sharing your information. ;-)")
